5265e908a99e3db6d044a78705fa08644239d5bd
Cawaz <3cawaz3@gmail.com>
2019-07-15 16:00:33 +0900

    コントローラの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,44 @@
-class PostsController < ApplicationController
+module Api
+  module V1
+    class PostsController < ApplicationController
+      before_action :set_post, only: %i[show update destroy]
+      def index
+        posts = Post.order(:created_at: :desc)
+        render json: { status: 'SUCCESS', message: 'loaded psots', data: posts }
+      end
+
+      def show
+        render json: { status: 'SUCCESS', message: 'loaded the post', data: post }
+      end
+
+      def create
+        post = Post.new(post_params)
+        if post.save
+          render json: { status: 'SUCCESS', message: 'loaded the post', data: post }
+        else
+          render json: { status: 'ERROR', message: 'post not saved', data: post.errors }
+        end
+      end
+
+      def destroy
+        post.destroy
+        render json: { status: 'SUCCESS', message: 'deleted the post', data: post }
+      end
+
+      def update
+        if post.update(post_params)
+          render json: { status: 'SUCCESS', message: 'updated the post' data: post }
+        else
+          render json: { status: 'SUCCESS', message: 'loaded the post', data: post }
+        end
+      end
+
+      private
+
+      def post_parmas
+        parmas.require(:post).permit(:title)
+      end
+    end
+  end
 end
+
